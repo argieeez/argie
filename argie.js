cf2cadaea0b8151b624829af83ce7ea879fbd635
argieeez <149215517+argieeez@users.noreply.github.com>
2023-11-20 23:10:40 +0800

    argie.js

---
+++ argie.js
@@ -0,0 +1,8 @@
+// script.js
+
+function openCurriculumVitae() {
+    // Replace the link with your actual Curriculum Vitae link
+    var curriculumVitaeLink = "https://drive.google.com/uc?export=view&id=1NtqK332_OTJ4yKtiQ3LAoQsKN_--ur0l";
+    window.open(curriculumVitaeLink, '_blank');
+}
+
